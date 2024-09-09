1b719b3b1f5524faf11573809e1fe6eb00a939d0
antonio craveiro <pe_1992@hotmail.com>
2024-09-09 21:19:19 +0200

    TSA-39: Changing database to postgres

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,14 +1,18 @@
-import mongoose from 'mongoose';
+import { Pool } from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URI, // You should have a POSTGRES_URI in your .env file
+});
+
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI as string);
-    console.log('MongoDB connected');
+    await pool.connect();
+    console.log('PostgreSQL connected');
   } catch (error) {
-    console.error('MongoDB connection error:', error);
+    console.error('PostgreSQL connection error:', error);
     process.exit(1);
   }
 };
