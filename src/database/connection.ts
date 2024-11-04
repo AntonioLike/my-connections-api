a10d5da4073111ab931c875e83ed69357c339b78
antonio craveiro <pe_1992@hotmail.com>
2024-11-04 18:00:37 +0100

    TSA-6: Now registers users on database

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,19 +1,15 @@
-import { Pool } from 'pg';
 import dotenv from 'dotenv';
+import { AppDataSource } from '../data-source';
 
 dotenv.config();
 
-const pool = new Pool({
-  connectionString: process.env.POSTGRES_URI, // You should have a POSTGRES_URI in your .env file
-});
-
 const connectDB = async () => {
   try {
-    await pool.connect();
-    console.log('PostgreSQL connected');
+    await AppDataSource.initialize();
+    console.log('PostgreSQL connected with TypeORM');
   } catch (error) {
-    console.error('PostgreSQL connection error:', error);
-    process.exit(1);
+    console.error('PostgreSQL connection error with TypeORM:', error);
+    process.exit(1); // Exit if the database connection fails
   }
 };
 
