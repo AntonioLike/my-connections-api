b582452cbc57d891c7564ebe293d26c09c2fddc3
antonio craveiro <pe_1992@hotmail.com>
2025-03-19 18:58:23 +0100

    TSA-8: Added new port to cors

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ const app: Application = express();
 
 // CORS configuration
 const corsOptions = {
-    origin: ['http://localhost:19006'], // Allow requests from frontend running on localhost:3000
+    origin: ['http://localhost:19006', 'http://localhost:8081'], // Allow requests from frontend running on localhost:3000
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
     credentials: true, // Allow cookies or authentication headers
 };
